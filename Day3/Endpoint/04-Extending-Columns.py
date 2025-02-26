a2c0bc760b9e556858396a912c717b95bd69da54
Daniel Rodríguez <webmaster@hotfixed.net>
2025-02-26 11:52:01 -0500

    Create 04-Extending-Columns.py

---
+++ Day3/Endpoint/04-Extending-Columns.py
@@ -0,0 +1,9 @@
+import re
+import pandas as pd
+df_base = pd.read_csv('sample_data/batilaptop-01.csv')
+columns_to_keep = ['Event Time','Action Type', 'File Name', 'Folder Path', 'Sha1', 'Sha256','MD5', 'Account Name',\
+                   'Initiating Process File Name', 'Initiating Process Folder Path','Process Command Line',\
+                   'Process Id', 'Initiating Process Folder Path', 'Initiating Process Parent File Name', 'Remote Url', 'Remote IP', 'Remote IP',	'Remote Port',	'Local IP',	'Local Port',\
+                   'File Origin Url',	'File Origin IP', 'File Origin Referrer Url', 'Categories','Severities']
+df = df_base[columns_to_keep]
+df.head()
